63170126db408bacc00ad2cfbef2ba4224226394
Joakim Petersson <petersson.joakim@gmail.com>
2021-11-27 09:34:06 +0100

    Updates

---
+++ component---src-pages-index-tsx-fe37224e9b50455b0f4d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_ts=self.webpackChunkgatsby_starter_ts||[]).push([[691],{6738:function(t,e,r){r.r(e);var s=r(7294),a=r(4774);e.default=function(){return s.createElement(a.Z,{title:"Joakim Petersson"},s.createElement("p",null,"A TypeScript starter for Gatsby. Great for advanced users."))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-fe37224e9b50455b0f4d.js.map
