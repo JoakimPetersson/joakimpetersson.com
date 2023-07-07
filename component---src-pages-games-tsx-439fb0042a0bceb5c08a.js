6fd6dc67e38c27138d13853675e91ae2726d7bfc
Joakim Petersson <petersson.joakim@gmail.com>
2023-07-07 15:41:20 +0200

    removed everything

--- component---src-pages-games-tsx-439fb0042a0bceb5c08a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_ts=self.webpackChunkgatsby_starter_ts||[]).push([[976],{9899:function(t,e,s){s.r(e),s.d(e,{default:function(){return n}});var r=s(7294),a=s(4774);function n(){return r.createElement(a.Z,{title:"Games"},"Games")}}}]);
-//# sourceMappingURL=component---src-pages-games-tsx-439fb0042a0bceb5c08a.js.map
--- component---src-pages-index-tsx-fe37224e9b50455b0f4d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_ts=self.webpackChunkgatsby_starter_ts||[]).push([[691],{6738:function(t,e,r){r.r(e);var s=r(7294),a=r(4774);e.default=function(){return s.createElement(a.Z,{title:"Joakim Petersson"},s.createElement("p",null,"A TypeScript starter for Gatsby. Great for advanced users."))}}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-fe37224e9b50455b0f4d.js.map
